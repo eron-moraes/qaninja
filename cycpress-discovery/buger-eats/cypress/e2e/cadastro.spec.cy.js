93e3b727ac34b286001ec0c3fb2fe87f9dda722a
Eron Moraes <eron.moraes7@gmail.com>
2023-05-27 11:40:19 -0300

    massadeteste

---
+++ cycpress-discovery/buger-eats/cypress/e2e/cadastro.spec.cy.js
@@ -0,0 +1,25 @@
+
+
+describe('Cadastro', ()=>{
+    it('Usuário deve se tornar um entregador', ()=>{
+        cy.viewport(1440,900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+            nome: 'Eron Moraes',
+            cpf: '00000000041',
+            email: 'eron@teste.com',
+            whatsapp: '12999999999'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+
+    })
+})
